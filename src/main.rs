6f8ee7590c1e7ca2d158d82fbdf3d1d46501a84e
ilya <antipakoman@gmail.com>
2024-09-17 21:38:53 +0300

    v2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::*;
+
+#[derive(Debug,Clone)]
+struct Tensor {
+	number: Vec<f32>,
+	dimension: Vec<usize>,
+}
+fn main(){
+	println!("123");
+	let tensor1 = Tensor {
+		number: vec![3.14, 2.5, 5.5, 5.6],
+		dimension: vec![2, 2],
+	};
+	let tensor2 = tensor1.clone();
+	println!("{:?}", tensor1);
+	println!("{:?}", tensor2);
 }
